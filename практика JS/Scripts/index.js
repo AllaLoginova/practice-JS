c381651e61c379bd57045b9cb86305e4a97b73f1
AllaLoginova <122536827+AllaLoginova@users.noreply.github.com>
2024-09-30 11:55:38 +0300

    Add files via upload

---
+++ практика JS/Scripts/index.js
@@ -0,0 +1,24 @@
+
+const title ='MegaSite';
+
+const navItems = ['Пункт 1', 'Пункт 2', 'Пункт 3' ];
+
+const bgcolor = 'DarkCyan';
+
+const fontcolor = 'white';
+
+function generateFooter (title, navItems, bgcolor, fontcolor) {
+
+    let result = ''
+    for (let i in navItems) {
+        result += `<button class="nav-item">${navItems[i]}</button>`;
+    }
+        
+    document.write(
+        `<div class="footer" style="background-color:${bgcolor}">`+
+        `<h1 id="title" style="color: ${fontcolor}">${title}</h1>`+
+        `${result}`+ `<div class="line-copyright"><pre>@${title.toLowerCase()}, ${new Date().getFullYear() + 3}</pre></div>` +
+       `</div>`)
+    }
+
+generateFooter(title, navItems, bgcolor, fontcolor)
